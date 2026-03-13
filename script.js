ca0166dd904c0028e20b2d48196f377d7fdd4b3d
tanzirbinalamin <tanzirbinalamin87@gmail.com>
2026-03-13 13:24:41 +0000

    updated

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-const login=document.getElementById("login-btn");
-const Password=document.getElementById("inputPass");
-const Username=document.getElementById("inputUser");
+const login = document.getElementById("login-btn");
+const Password = document.getElementById("inputPass");
+const Username = document.getElementById("inputUser");
+const user = document.getElementById("userStat");
+const pass = document.getElementById("passStat");
+
+login.addEventListener("click", function() {
+
+    if (Username.value === "admin" && Password.value === "admin123") {
+        alert("Login success");
+        window.location.assign("home.html");
+    } 
+     else if(Username.value != "admin") {
+        user.innerHTML=`
+          <p id="userStat" class="text-red-500 font-bold">*incorrect username</p>
+        `
+    }
+
+    else if(Password.value != "admin123") {
+        pass.innerHTML=`
+          <p id="userStat" class="text-red-500 font-bold">*incorrect password</p>
+        `
+
+        
+    }
+
+});
+
+ 
